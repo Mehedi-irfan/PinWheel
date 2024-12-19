73d93ca83a9781a2a7ce9aff5eb1a2ed6f277548
Mehedi-irfan <mehediirfan99@gmail.com>
2024-12-19 17:45:22 +0600

    Initilize the project

---
+++ tailwind.config.js
@@ -0,0 +1,44 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    screens: {
+      sm: "340px",
+      md: "768px",
+      lg: "968px",
+      xl: "1250px",
+    },
+    colors: {
+      "color-white": "#fff",
+      "color-text": "rgb(34 34 34 )",
+      "color-bg": "rgb(255, 255, 255)",
+      "color-gray": "#888888",
+      "color-highLight": "rgb(254 96 25 / 0.4)",
+      "color-primary": "rgb(254 96 25 )",
+    },
+    backgroundImage: {
+      "color-gradient":
+        "linear-gradient(184.78deg, #fa709a 7.64%, #fee140 120.07%)",
+      "header-img": "url('./imgaes/header-banner-img.png')",
+      "header-shape": "url('./imgaes/header-banner-shape.svg')",
+      "hightLight-shape": "url('./imgaes/highlight-section-shape.svg')",
+      "circle-shape": "url('./imgaes/circle-shape.svg')",
+      "sellsCountry-img": "url('./imgaes/sells-by-country-right-img.png')",
+      "collaboration-img": "url('./imgaes/collaboration-img.png')",
+      "thumbnail-img": "url('./imgaes/intro-thumbnail.png')",
+    },
+    fontFamily: {
+      jost: ["Jost", "serif"],
+      lobster: ["Lobster", "serif"],
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "10px",
+        md: "30px",
+      },
+    },
+    extend: {},
+  },
+  plugins: [],
+};
