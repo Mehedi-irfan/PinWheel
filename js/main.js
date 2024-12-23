5d6e9cc69cb4b91a40ad93608c128f07bc1dd009
Mehedi-irfan <mehediirfan99@gmail.com>
2024-12-23 17:30:17 +0600

    slider section added

--- js/main.js
+++ js/main.js
@@ -14,3 +14,35 @@ navLink.forEach((link) => {
     hamburg.classList.toggle("ri-close-large-line");
   });
 });
+
+// swiper
+document.addEventListener("DOMContentLoaded", () => {
+  const swiper = new Swiper(".swiper", {
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    autoplay: {
+      delay: 3000,
+    },
+    // Add responsive breakpoints
+    breakpoints: {
+      // When the screen width is >= 1024px (large devices)
+      1024: {
+        slidesPerView: 3, // Show 4 slides
+        spaceBetween: 40, // Space between slides in px
+      },
+      // When the screen width is >= 768px (medium devices)
+      768: {
+        slidesPerView: 2, // Show 2 slides
+        spaceBetween: 15, // Space between slides in px
+      },
+      // When the screen width is < 768px (small devices)
+      0: {
+        slidesPerView: 1, // Show 1 slide
+        spaceBetween: 10, // Space between slides in px
+      },
+    },
+  });
+});
